1c384b283c59d65555b5238ffa6ce28ca4708dbe
MoneerMadwar <naeroo.shadow11@gmail.com>
2025-04-26 04:15:59 +0300

    Delete WebApplication1 directory

--- WebApplication1/Program.cs
+++
@@ -1,7 +0,0 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
-
-app.UseDefaultFiles(); // íÎáí ÇáãæŞÚ íİÊÍ index.html ÊáŞÇÆíÇğ
-app.UseStaticFiles();  // íÓãÍ ÈÚÑÖ ãáİÇÊ HTML æCSS æJS
-
-app.Run();
